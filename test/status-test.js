e7a016f2e8fc2e5f9775e516568e529f26f8557b
Eric Skoglund <eric.skoglund@expressen.se>
2017-04-15 15:47:12 +0200

    Added chai and test for response of /_status

--- test/status-test.js
+++ test/status-test.js
@@ -1,14 +1,24 @@
 "use strict";
 
 const request = require('supertest');
+const should = require('chai').should();
 
 const app = require('../app');
 
 Feature('_status', () => {
+  let response;
+
   When('Requesting /_status the app responds with a 200 OK', (done) => {
     request(app)
       .get('/_status')
       .expect(200)
-      .end(done);
+      .end((err, res) => {
+        response = res.text;
+        done();
+      });
+  });
+
+  And('Result is OK', () => {
+    response.should.eql(JSON.stringify({ status : 'OK'}));
   });
 });
